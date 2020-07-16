ae56ec1a4a940ca0e757038de5fde952917d3ec5
Erasmo Sena <erasmo.sena@gmail.com>
2020-07-16 18:57:15 -0300

    html

--- Redes Neurais/Feedforward/sketch.js
+++ Redes Neurais/Feedforward/sketch.js
@@ -1,11 +1,23 @@
+var canvas
+
+function centerCanvas() {
+  debugger
+  var x = (canvas.parent().clientWidth - width) / 2;
+  var y = (canvas.parent().clientHeght - height) / 2;
+  canvas.position(x, y);
+}
+
 function setup() {
-    createCanvas(500, 500);
-    background(0);
-    var m1 =  new Matrix(1,2);
-    var m2 =  new Matrix(2,1);
-    Matrix.produtoInterno(m1,m2);
-  }
-  
-  function draw() {
-    
-  }
+  canvas = createCanvas(500, 500);
+  canvas.parent('container');
+  centerCanvas();
+
+  background(0);
+  var m1 = new Matrix(1, 2);
+  var m2 = new Matrix(2, 1);
+  Matrix.produtoInterno(m1, m2);
+}
+
+function draw() {
+
+}
